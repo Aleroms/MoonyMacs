e4399bd14a6f2294de69aaaf11a3bbbf7e934cde
Santiago <santiagomorales333@yahoo.com>
2024-07-09 18:43:50 -0700

    added stuff to footer. plus completed home page w/o needed styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,30 +5,37 @@ import Logo from "../Logo/Logo";
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <div className="footer-item">
-        <p>stay updated</p>
-        {/* insert social media container here  */}
-      </div>
-      <div className="footer-item">
-        <nav>
-          <NavbarLinks />
-        </nav>
-      </div>
-      <div className="footer-item">
-        <div className="footer-stack">
-          <div className="stack-item">
-            <h4>Service Area</h4>
-            <p>
-              My current service area is the High Desert and surrounding areas.
-            </p>
-          </div>
-          <div className="stack-item">
-            <Logo width={100} />
+    <>
+      <footer>
+        <div className="footer-item">
+          <p>stay updated</p>
+          {/* insert social media container here  */}
+        </div>
+        <div className="footer-item">
+          <nav>
+            <NavbarLinks />
+          </nav>
+        </div>
+        <div className="footer-item">
+          <div className="footer-stack">
+            <div className="stack-item">
+              <h4>Service Area</h4>
+              <p>
+                My current service area is the High Desert and surrounding
+                areas.
+              </p>
+            </div>
+            <div className="stack-item">
+              <Logo width={100} />
+            </div>
           </div>
         </div>
+      </footer>
+      <div className="footer-credits">
+        To the Moon and Macs © | Designed by{" "}
+        <a href="https://santiagomorales.dev/">Santiago Morales</a>
       </div>
-    </footer>
+    </>
   );
 };
 export default Footer;
