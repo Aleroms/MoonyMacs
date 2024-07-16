10f43903ef075ce8bdb98be47635b8131ebd593a
Santiago <santiagomorales333@yahoo.com>
2024-07-15 22:32:55 -0700

    basic gallery component

---
+++ src/components/GalleryComponent/GalleryCompontent.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./GalleryComponent.css";
+
+interface Image {
+  id: number;
+  path: string;
+  alt: string;
+}
+interface GalleryProps {
+  images: Image[];
+}
+
+const GalleryComponent: React.FC<GalleryProps> = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+
+  const handleClose = () => {
+    setSelectedImage(null);
+  };
+
+  const handleImageClick = (image: Image) => {
+    setSelectedImage(image);
+  };
+
+  return (
+    <section className="gallery-container">
+      <div className="gallery-grid">
+        {images.map((image) => (
+          <div
+            className="gallery-item"
+            key={image.id}
+            onClick={() => handleImageClick(image)}
+          >
+            <img src={image.path} alt={image.alt} />
+          </div>
+        ))}
+
+        {selectedImage && (
+          <div className="modal" onClick={handleClose}>
+            <span className="close">&times;</span>
+            <img
+              className="modal-content"
+              src={selectedImage.path}
+              alt={selectedImage.alt}
+            />
+            <div className="caption">{selectedImage.alt}</div>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default GalleryComponent;
