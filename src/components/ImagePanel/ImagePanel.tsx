24abb56b76b7e5cafad3ac993cf4e2324aac4fc8
Santiago <santiagomorales333@yahoo.com>
2024-07-27 20:55:22 -0700

    added images to image panel

--- src/components/ImagePanel/ImagePanel.tsx
+++ src/components/ImagePanel/ImagePanel.tsx
@@ -1,24 +1,19 @@
 // import React, { useEffect, useState } from "react";
 import React from "react";
 import "./ImagePanel.css";
+import { images } from "../../data/imagePanel.json";
 
-// EVENTUALLY HAVE IMAGE PATHS BE PASSED IN PROPS AND RENDER IT USING MAP
 const ImagePanel: React.FC = () => {
-  const width = 200;
-  //   const [windowWidth, setWindowWidth] = useState(40);
+  const imageWidth = 200;
 
-  //   useEffect(()=>{
-  //     const windowWidthHandler = () =>{
-
-  //     }
-  //   },[])
   return (
     <section className="image-panel">
       <div className="image-row">
-        <img src="/macarons/orange-mac.png" alt="orange macron" width={width} />
-        <img src="/macarons/orange-mac.png" alt="orange macron" width={width} />
-        <img src="/macarons/orange-mac.png" alt="orange macron" width={width} />
-        <img src="/macarons/orange-mac.png" alt="orange macron" width={width} />
+        {images.map((image, idx) => (
+          <div className="img-panel-wrapper">
+            <img src={image.src} alt={image.alt} width={imageWidth} key={idx} />
+          </div>
+        ))}
       </div>
     </section>
   );
